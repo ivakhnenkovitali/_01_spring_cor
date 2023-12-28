e66fe9317d274c00333eff02da8d429fc89bbb9e
Asus <vaxa2320@gmail.com>
2023-12-28 19:52:36 +0300

    user

---
+++ src/main/java/_01_base/Main.java
@@ -0,0 +1,11 @@
+package _01_base;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+       var ctx = new ClassPathXmlApplicationContext("01_helloworld.xml");
+       var message = ctx.getBean("default-message", String.class);
+        System.out.println(message);
+    }
+}
