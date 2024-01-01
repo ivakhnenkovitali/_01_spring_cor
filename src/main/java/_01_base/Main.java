4e9461aecf89e3d333dd1e0f8f6cfb7783c6e7b4
Asus <vaxa2320@gmail.com>
2024-01-01 15:23:18 +0300

    user

--- src/main/java/_01_base/Main.java
+++ src/main/java/_01_base/Main.java
@@ -5,9 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         var ctx = new ClassPathXmlApplicationContext("01_helloworld.xml");
-        ///
+       ///// наш контекст будет описон в xml файле- коробочка.
+
+
         var message = ctx.getBean("message", String.class);
         System.out.println(message);
     }
 }
-///предоставляет комплексную модель
--- src/main/java/_02_car/SimpleCar.java
+++ src/main/java/_02_car/SimpleCar.java
@@ -1,23 +1,53 @@
 package _02_car;
 
+import java.util.StringJoiner;
+
 public class SimpleCar {
     private int id;
     private String model;
 
 
-
     public SimpleCar() {
-        model = "model";
+        model = "Default model";   /// пустой конструктор
     }
 
-    public SimpleCar(String model) {
+
+    public SimpleCar(String model) {  //// с одним пораметром
         this.model = model;
     }
 
-    public SimpleCar(String model, int id) {
+    public SimpleCar(int id, String model) {  ///// с двумя пораметрами
         this.id = id;
         this.model = model;
     }
 
 
+    public SimpleCar(String model, int id) {   ////// с двумя пора-ми только меняем .... содержание наоборот
+        this.id = id;
+        this.model = model;
+    }
+
+    public int getId() {    /// гет сет
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override     //// тостринг
+    public String toString() {
+        return new StringJoiner(", ", SimpleCar.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("model='" + model + "'")
+                .toString();
+    }
 }
